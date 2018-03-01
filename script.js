e044649da5fa8d950ddd6599e0791c6bd5214dea
Theo95400 <theo.gonzalez69@gmail.com>
2018-03-01 09:55:53 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+function saut() {
+$("#flappy").animate({marginBottom: '+=100px'}, 100, "swing");
+}
+
+function deplacement_vertical (sens){
+    if(sens == 1 ){
+        deplacement = "+=100px"
+    }
+    else{
+        deplacement = "-=100px"
+    }
+    $("#flappy").animate({ "marginTop": deplacement
+    },500,"swing");
+    
+}
+
+
+
+function bougerHaut(){
+$("#flappy").animate({marginBottom:'+=100px'}, speed=200, easing="swing");
+     }
+
+function bougerBas(){
+$("#flappy").animate({marginTop:'+=100px'}, speed=200, easing="swing");
+     }
+
+function bougerGauche(){
+$("#flappy").animate({marginRight:'+=100px'}, speed=200, easing="swing");
+     }
+
+function bougerDroite(){
+$("#flappy").animate({marginLeft:'+=100px'}, speed=200, easing="swing");
+     }
+
+
+
+
+
+$(document).keyup(function(touche){
+           var appui = touche.which || touche.keyCode;
+           if(appui == 38){
+               bougerHaut();
+           }
+});
+
+
+$(document).keyup(function(touche){
+            var appui = touche.which || touche.keyCode;
+            if(appui == 40){
+                bougerBas();
+           }
+});
+
+
+$(document).keyup(function(touche){
+            var appui = touche.which || touche.keyCode;
+            if(appui == 37){
+                bougerGauche();
+           }
+});
+
+
+$(document).keyup(function(touche){
+            var appui = touche.which || touche.keyCode;
+            if(appui == 39){
+                bougerDroite();
+           }
+});
